3b0ee8ba7bf23f1e09c238d9d97e53e3cfd42773
Keith Gudger <kgudger@gmail.com>
2014-07-08 21:54:00 -0700

    First stab at both lists, still need line breaks.

---
+++ www/js/index_user_scripts.js
@@ -0,0 +1,21 @@
+(function()
+{
+ "use strict";
+ /*
+   hook up event handlers 
+ */
+ function register_event_handlers()
+ {
+    
+    
+         $(document).on("click", ".uib_w_3", function(evt)
+        {
+         activate_subpage("#mainsub"); 
+        });
+        $(document).on("click", ".uib_w_4", function(evt)
+        {
+         activate_subpage("#natsp"); 
+        });
+}
+ $(document).ready(register_event_handlers);
+})();
